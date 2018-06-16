e314ed23a18da86bca3de22ebdd9e54058f093bf
dicenoir84 <40320813+dicenoir84@users.noreply.github.com>
2018-06-16 11:20:51 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Dice Noir Media</title>
+<link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+
+<div id="top">
+<h1>Dice Noir Media</h1>
+
+<nav>
+
+<ul>
+	<li><a href="#">About</a></li>
+    <li><a href="#">Video Productions</a></li>
+    <li><a href="music.html">music</a></li>
+    <li><a href="graphics.html">Graphic Designs</a></li>
+    <li><a href="index.html">Home</a></li>
+    
+    
+ </ul>
+ </nav>
+
+<img src="Pics/Dice Banner.jpg" width="1025">
+
+</div>
+
+<main>
+
+<div id="categories"> <a href="graphics.html"><img src="The Camelot Knightly.jpg" width="700" height="392" class="graphics"></a>
+
+</div>
+
+<div id="next">
+
+<h2>A One knight Affair</h2>
+
+<p>
+
+Camelot, I have a bit of butter to spread in thine ear.  For there are whispers from on high this night.  A seductive and conspirators spirit has manifested in the very heart of Camelot.  I tell you the truth that this is a strong word I tell you, and yet I am compelled to do so even at my own peril.  I do so in the service of King Arthur Pendragon Ruler of Great Britain.  There is a saying, “there is a distinct, awful pain that comes with loving someone more than they love you.”  And I fear that the damage done to the heart will reflect in the minds of those within the kingdom. 
+Suspicion and intrigue has swollen greatly in the kingdom following the duel between Sir Lancelot and Sir Meliagaunt. Sir Meleagaunt accused Queen Guinevere of treason.  The Queen denied all allegations and held Sir Meleagaunt under contempt. The rumor attracted great attention throughout all of Camelot as the duel was announced eight days ahead.  The entire kingdom gathered to West Minister where just along its borders, lay a field.   The King held court on the matter on the day after the lord 840AD.  Of course, we all now the events as we were all summoned to the hearing.  Sir Meleagaunt was completely sure of his suspicions that hot Fall day in September.  For some reason it seemed as though the sun was giving testimony to bring light to what was suspected to be done in the dark.  We all stood in the open court of Camelot.  The court was silent as all ears wagged to hear this accusation and how deep this darkness goes.  
+King Arthur begins,” Sir Meliagaunt, you stand on the eve of your battle with Sir Lancelot.  The very matter of your duel is the reason I, King Arthur Pendragon, ruler of Britannia, sanctioned this hearing.  Make your case known before the people against the Queen.”
+Sir Meleagaunt,” My King, I but am doing my duty informing all of Camelot of that which have transpired.  I know what I saw.  I have been loyal even when others thought me a traitor.  I have never been false even as I have been called a serpent.  And all knights of the round table may confirm my claim.  Blood in her bed, not of her own.”
+This was the single most shocking claim that we had heard come from the kingdom.  What was the meaning of such a display and why.  To some it was heart breaking.  We have always known Queen Guinevere to be humble, contrite, and gentle natured.  She has graced the side of King Arthur for over a decade and has never looked at another the way she looked at him.  
+Sir Meleagaunt continues, “I have held Queen Guinevere under the protection of my castle and have also housed the knight of the table round.  It has served to hold at bay, the forces of Morgan and her armies in the southern providence.  An assassination attempt was made upon the Queen as she walked through the lands to greet the people.  I along with Gawain, Percival, Mordred, and Lancelot escorted the Queen as we were hard pressed in an ambush.  They were skilled in the art of shadow and silence.  They wounded us in battle yet were no match for knights of the table round.  At there vanquishing we returned immediately back to the castle were Guinevere retreated to her chambers.  There she stayed all the rest of the day, even through the night as the men tended to their duties. I came upon Queen Guinevere’s chamber door to ensure her security as she did not give presence at all after the attempt.  And when I had entered, I had seen it. There had been blood upon her sheets, and pillows.  At that moment I had found in Queen Guinevere disloyalty to the King.  A knight of the table round has been in bed with the queen.  She is false.”
+Now there was not one word spilled and the air was so thick with emotional intrigue, you couldn’t help but breath it in.  All of the kingdom loves Queen Guinevere and give their lives in service to her majesty.  She has been a guiding light to the people of Camelot and a voice of wisdom and strength to it’s inhabitants.  There has not been a stronger and exemplary union between man and wife than that of King Arthur and Queen Guinevere.  The anger and confusion swelled the courtyard as Sir Lancelot stood to defend the honor of the Queen.
+Sir Lancelot makes a rebuttal, “My king, it is not a position of a knight to draw the curtains of the queen’s bed.  He has violated the queen in this manner.  How do we know that his own hands have not stained the bed, for it was sir Meleagaunt who made the discovery and he first to discover the blood upon it?”
+Sir Meleagaunt defends, “Ah Jesus mercy, does even you Sir Lancelot, the greatest of the knights even think of me false even after I have fought by your side? Are we not brothers?  Then I shall give witness of my God that he delivers me.  I swear upon all that is holy and true by that which I have spoken.”
+Then sir Lancelot interrupts screaming his final warning to his fellow knight. 
+ “Meleagaunt!”
+All is still.  Not one joint was bent, nor any eye present blinked, nor finger twitched, nor blowing of the wind, nor even a pin drop.  His voice echoed through the air.  And a dark cloud covered the sun in the presence of his voice, shrouded in omen and mystery.  And as this cloud past, there still standing, feet planted as though on a rock upon a mountain was Sir Meleagaunt still ever defiant.  
+Sir Lancelot approaches sir Meleagaunt with such a vicious look on his face.  It was if he were ready to swing his sword to murderous purpose.  “Beware what you do, for if you are ready to prove it with your hands it will be taken at your hands.”
+“And I’m telling you Sir Lancelot, “said sir Meliagaunt, “beware what you do.  You’re never that good of a knight or so renowned to do battle in the wrong quarrel for God would have stroke in every battle. “
+“As for that, God is to be dreaded.  But I tell you that none of theses knight have defiled our Queen.  I will prove you false Meleagaunt with mine own hands.
+And with these words expressed, King Arthur adjourned, and permitted the duel between Sir Lancelot and Sir Meleagaunt.  Should Meleagaunt win, the queen would be considered false to the king and burned on a pyre.  If Sir Lancelot best Meleagaunt, the Queen is proven innocent.  Eight days they prepared to face each other.  Fellow knights pleaded with meleagaunt to desist his allegation but to no avail, for his resolve was set in stone and there upon it, etched with burning rage.  And the morning of the duel, there was such an anxious feeling in Camelot.  Every soul was anchored towards this bout.  Two knights of the table round.  Among all the knights of the world, the knights of King Arthur were renowned for their fighting spirits and brotherhood.  Not one knight above another, their bond was their shield.  They had never been defeated nor bested by any adversary.  For two of the knights of King Arthur to duel in mortal combat would truly be a historic moment in the history of Britannia, and of Camelot.
+The king and his court rose early to meet the dawn.  The knights of Camelot gathered their along with the king to pray together that God may have an eye to this quarrel.  And people from all over Camelot began to flood Minister field.  Queen Guinevere watches intensely at the spectacle.  Her golden hair sways to the rhythm of the wind.  Her eyes narrowing to hone in on her defender and finding him not.  Sir Meleagaunt rides on to the scene on his midnight horse.  The crows herald his entrance and flew over his head cawing the announcement of his arrival.  He stood alone awaiting Sir Lancelot to join the quarrel.  
+“Where is Lancelot, “said Sir Meleagaunt as he gallops the field.  “Where is the knight that has sworn to fight for the honor of the queen.  If he be not here, then his honor and her honor are forfeit.”
+“It is a good thing then that I have arrived to this field of blood, “said Lancelot as he arrives on his white as snow steed saliphax the wild horse of the mystic lake.  “I have joined thee in combat Sir Meleagaunt.  This your last chance to lay down arms and come to reason.  Avoid this quarrel and reason with your brothers.  If it pleases thee however that we do battle this day know that no quarter shall be given.”
+Sir Meleagaunt takes hold of spear and lowers his helmet which then signals to Sir Lancelot that he is prepared to do battle and says, “I say to you Sir Lancelot have at thee.”  
+Sir Meleagaunt charges towards Sir Lancelot, spear drawn, prepared to separate him from Saliphax.  At that very moment, Sir Lancelot charges as well as Sir Gawain throws him his spear.  The two dueling knights charge at each other, picking up speed with every inch drawn nearer to each other.  Perfectly focused on their targets, the spears do all the works in favor of their masters.  Lancelot and Meleagaunt strike each other one injuring the left shoulder, one injuring the ribs.  As they pass each other in pain their mortality drew ever closer and their pain great.  Yet, they press on, each convicted of their arguments, each with something to prove.  They strike and collide with each of them, a great dragon sitting on their shoulders.  The crowd watches on dazzled and amazed at the resolve and dedication of these two warriors.  How they shouted that day.  Their jeers and praise reached all through Britannia and all of its borders.  Sir Meleagaunt began to fade, his vision began to blur, the blows now reaching deep into the core of his mental stability to maintain his resolve.  Lancelot cries out in battle at Sir Meleagaunt as a berserker gone into blood lust.  He Meleagaunt begins to slow his pace. Sir Lancelot, focused on his target thrust his spear about the area of his ribs and finally sperate Sir Meliagaunt from his horse.  Sir Meleagaunt fell.  Lancelot, once seeing his dominance over his opponent, removed himself from his horse and drew his sword disregarding the spear.  And when Sir Meliagaunt saw him he was afraid for his life would not be spared.
+“I yield, “cried Sir Meliagaunt.  “Slay me not.  Thou hast bested me in fair combat.  I shall take back my words.  Queen Guinevere is innocent.”
+Sir Lancelot paused and as so much as the crowd.  We were all anticipating his decision.  His look was that of moral consciousness.  He didn’t know what to do.  He glanced towards the queen to make this decision, for her honor and life hangs on this duel’s end.  Queen Guinevere wags her head at Sir Lancelot as to say, “slay him.” At that moment Sir Lancelot’s focused returns back to Sir meliagaunt.  Reassured to his purpose he is ready to deliver the accuser into deaths arms.  Sir Lancelot stands over Meleagaunt in disgust for this knight once filled of pride and violence now reduced to the one thing no man should ever fear should he be righteous with God as such the honor of a knight stands upon. “you put yourself to shame Meleagaunt.  Rise to your feet and see this quarrel to its end.”
+“I will not rise until you have taken me as yielded and recreant,” said sir Meliagaunt as he coward under the shadow of Lancelot.”
+Lancelot lowers his sword and loosens his helmet to remove it.  He then removes the armor on his left side.
+“You’re not worthy of being a knight, and you don’t deserve to die like one.  You will, however face me Meleagaunt.  What say you then?  I have exposed the left quarter of my body and removed my helmet.  I shall also bind my left hand.  In this way I shall continue to battle, if it so pleases the king.”
+King Arthur speaks with such an authority and power, “what say you Sir Meleagaunt?”
+“Take heed my lord of this offer that I will take it.”
+“Very well then.  See this battle to it’s end.  May God have mercy upon your souls.”
+And Sir Lancelot’s left hand was bound behind his back.  Sir Meleagaunt rose to his feet and drew his sword.  Likewise, Sir Lancelot takes his stance.  Sir Meleagaunt charges like a bull that see’s red in front of him.  Sir Lancelot showed him openly his body partly stripped of armor on his left and his head.  Sir Meleagaunt moves to strike his temple.  Lancelot steps ever so slightly to the left just avoiding the blow and positioning himself behind Meleagaunt.  Lancelot shifts his grip on the hilt of his sword and bludgeons the back of Sir Meleagaunt’s head with such a force that Sir Meliagaunt helmet discharged from his head.  And with a swift rotation of sword, came down on Sir Meleagaunt’s head, splitting it in two.
+  “Exquisite.”
+  There was no more to do after that.  Sir Meleagaunt was slain and Queen Guinevere was proven innocent.
+That was only a few days ago.  We rejoiced the victory of Sir Lancelot, and faith in the queen strengthened.  Yet still suspicions are still swirling across the kingdom.  Sir Meleagaunt left a stigma in the wake of his death.  Many are asking themselves, what would make this man go this far just to give false witness.  Of course, some believe in a conspiracy, that there are those in the kingdom that secretly have ties to Morgan Le Fay, the Witch Queen.  The rumor is this, since the last battle fought against the witch queen, Guinevere has been different.  She has seen her fair share of battles and rode hard into battle along side the king.  But she has been more settled and gentler natured in her approach.  This could be due to relieving those times on the battle field and wanting to leave it behind her, or it could be that she was altered by more mystical forces.  Either way, the scenario that transpired definitely lines up with the altered state of Queen Guinevere.  Yes, this bard has a feeling that this tale is not ended.  Further details will be revealed as this story develops.  Keep an open ear Camelot.  Spread the butter on thick.
+</p>
+
+</div>
+
+</main>
+
+</body>
+
+<div id="copyright">
+
+&copy;dice noir media 2018
+
+</div>
+
+</html>
